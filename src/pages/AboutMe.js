a878d0965b324aefa895a1ce9ca259cea22a1cfd
theTurtl <addimanni@gmail.com>
2022-10-24 19:35:59 +0200

    Most of HTML structure done.

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,9 +6,108 @@ import Seo from "../components/seo"
 
 const AboutMe = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className="aboutMePage">
+      <div className="landingPageNavBar">
+        <div className="navLeft">
+          <div className="welcomeContainer">
+            <p>Hyggelig å treffe deg! :)</p>
+          </div>
+          <div className="availabilityStatus">
+            <p>Ledig</p>
+            <div className="availabilityIcon"></div>
+          </div>
+        </div>
+        <div className="navMiddle">
+          <div className="knowledgeButton">
+            <p>Min kunnskap</p>
+            <div className="accentLine"></div>
+          </div>
+          <div className="aboutButton">
+            <p>Om meg</p>
+            <div className="accentLine"></div>
+          </div>
+        </div>
+        <div className="navRight">
+          <div className="languageMenu">
+            <p>NO</p>
+            <div className="arrowDown"></div>
+          </div>
+          <div className="contactButton">
+            <p>Kontakt</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="aboutMeBanner">
+        <div className="aboutMeBannerLeft">
+          <p>Heisann :)</p>
+          <p>Her står det litt om hvem jeg er, og<br/>
+          hvordan jeg endte opp med frontend og<br/>mobilutvikling.</p>
+          <div className="introductionButtons">
+            <div className="downloadCV">
+              <p>Last ned CV</p>
+            </div>
+            <div className="portfolio">
+              <div className="arrivingSoon">
+                <p>Kommer</p>
+              </div>
+              <p className="portfolioText">Portfolie</p>
+            </div>
+          </div>
+        </div>
+        <div className="aboutMeBannerRight">
+          <div className="roundPortrait"></div>
+        </div>
+      </div>
+
+      <div className="whoAmISection">
+        <h4>Hvem er jeg?</h4>
+        <div className="whoAmIDivider">
+          <div className="whoAmILeft">
+            <div className="partOne">
+              <h6>Part 1</h6>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+                sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+            </div>
+            <div className="partTwo">
+              <h6>Part 1</h6>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+                sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+            </div>
+            <div className="partThree">
+              <h6>Part 1</h6>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+                sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+            </div>
+          </div>
+          <div className="whoAmIRight">
+            <div className="quoteSection">
+              <div className="header">
+                <div className="headerIllustration"></div>
+                <p>Nicole a. Dkoea</p>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+                sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+            </div>
+            <div className="statusSection">
+              <div className="statusHeader">
+                <p>Status</p>
+                <div className="statusIcon"></div>
+              </div>
+              <div className="statusContainer">
+                <p>Student på 3. året i frontend og mobilutvikling bachelor.</p>
+              </div>
+            </div>
+            <div className="myInterestSection">
+              <p>Mine interesser</p>
+              <div className="pointsField">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
