78fce7b9fed93fce50650dca7aab9365d9d7a29f
theTurtl <addimanni@gmail.com>
2022-10-25 15:59:08 +0200

    Added API for getting font from google.

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Quicksand`,
+            file: `https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap`,
+          },
+        ],
+      },
+    },
   ],
 }
