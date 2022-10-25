eff3750475823a0af36ed394c8ffbafabc138d93
theTurtl <addimanni@gmail.com>
2022-10-25 17:45:12 +0200

    Started optimizing for different resolutions. Soon done. I need food!

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import NavBar from "../components/navBar"
+import NavBar from "../components/NavBar"
 
 const AboutMe = () => (
   <Layout>
