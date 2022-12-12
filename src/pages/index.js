6e68dacd26668a3aae59b90dd7a2cb7aea963c32
theTurtl <addimanni@gmail.com>
2022-12-12 02:12:19 +0100

    Deleted unused .js files.

--- src/pages/index.js
+++ src/pages/index.js
@@ -243,6 +243,6 @@ const IndexPage = () => {
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Hjem" />
+export const Head = () => <Seo title="Adrian Aspevik" />
 
 export default IndexPage
